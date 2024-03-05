a5d6e69033c46087c6b62e2a3fb092e3e547beb8
Jesus <jesus.rangel@cceo.com.mx>
2024-03-05 10:48:28 -0600

    feat: :green_heart: routes file, models for routes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,10 +6,23 @@ class HomeScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+    return  Scaffold(
+      appBar:  AppBar(
+        title: const Text("Componentes en flutter"),
+        elevation: 0,
+        backgroundColor: Colors.indigoAccent,
       ),
+      body: ListView.separated(
+        itemBuilder: (context, index) => 
+        ListTile(
+          leading: const Icon(Icons.av_timer_rounded),
+          title: const Text('nombre de ruta'),
+          onTap: (){
+            Navigator.pushNamed(context, 'card');
+          },
+        ), 
+        separatorBuilder: ( _ , __) => const Divider() , 
+        itemCount: 10)
     );
   }
 }
