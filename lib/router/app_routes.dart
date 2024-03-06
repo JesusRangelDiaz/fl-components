2efa462ea9c74f3cc5729fb49df037d5b81b9514
Jesus <jesus.rangel@cceo.com.mx>
2024-03-06 15:02:34 -0600

    feat(new screens): :sparkles: animated_screen & input_screen

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -8,17 +8,21 @@ class AppRoutes {
   static const intitialRoute = 'home';
 
   static final menuOptions = <MenuOption>[
-    MenuOption(route: 'home', icon: Icons.home, name: 'Home Screen', screen: const HomeScreen()),
-    MenuOption(route: 'listview1', icon: Icons.list, name: 'ListView 1', screen: const Listview1Screen()),
-    MenuOption(route: 'listview2', icon: Icons.list_alt, name: 'ListView 2', screen: const Listview2Screen()),
-    MenuOption(route: 'alert', icon: Icons.dangerous_sharp, name: 'alert', screen: const AlertScreen()),
-    MenuOption(route: 'card', icon: Icons.credit_card, name: 'card', screen: const CardScreen()),
-    MenuOption(route: 'avatar', icon: Icons.person_rounded, name: 'avatar', screen: const AvatarScreen()),
+    //MenuOption(route: 'home', icon: Icons.home, name: 'Home (Pagina Principal)', screen: const HomeScreen()),
+    MenuOption(route: 'listview1', icon: Icons.list, name: 'ListView Tipo 1', screen: const Listview1Screen()),
+    MenuOption(route: 'listview2', icon: Icons.list_alt, name: 'ListView Tipo 2', screen: const Listview2Screen()),
+    MenuOption(route: 'alert', icon: Icons.notification_important, name: 'Alertas - Alerts', screen: const AlertScreen()),
+    MenuOption(route: 'card', icon: Icons.credit_card, name: 'Tarjetas - Cards', screen: const CardScreen()),
+    MenuOption(route: 'avatar', icon: Icons.person_outline_outlined, name: 'Circle Avatar', screen: const AvatarScreen()),
+    MenuOption(route: 'animated', icon: Icons.animation, name: 'Animated Container', screen: const AnimatedScreen()),
+    MenuOption(route: 'inputs', icon: Icons.input_outlined, name: 'Text Inputs', screen: const InputScreen()),
   ];
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes(){
     
     Map<String,Widget Function(BuildContext)> appRoutes={};
+    appRoutes.addAll({'home':(BuildContext context)=>const HomeScreen()});
+
       for (final option in menuOptions){
         appRoutes.addAll({option.route : (BuildContext context) => option.screen});
       }
